5a084f39be7e9eb64d28484eb8cf9e6073b408bb
anonanonymous <23041331+anonanonymous@users.noreply.github.com>
2018-11-12 18:15:39 -0800

    update schema

--- userdb.sql
+++ userdb.sql
@@ -1,10 +1,11 @@
 CREATE DATABASE userdb_test;
 \c userdb_test
 CREATE TABLE users (
-id serial primary key,
-ih char(64) not null,
-verifier char(585) not null,
-username varchar(64) not null,
-email varchar(128) default null,
-privateKey char(128) not null,
-totpKey char(102) default null);
+	id serial primary key,
+	ih char(64) not null,
+	verifier char(585) not null,
+	username varchar(64) not null,
+	email varchar(128) default null,
+	privateKey char(64) not null,
+	totpKey char(64) default null
+);
